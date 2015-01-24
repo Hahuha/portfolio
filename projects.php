2477437b3bdcb157029f46369837af2630eb39c1
Carvallegro <hahu-huha@hotmail.fr>
2015-01-24 17:45:37 +0100

    Clean up
    Suppression des fichiers inutiles

--- projects.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once('application/mustache/src/Mustache/Autoloader.php');
-Mustache_Autoloader::register();
-$mustache = new Mustache_Engine(array(
-'loader' => new Mustache_Loader_FilesystemLoader(dirname(__FILE__).'\application\templates')
-));
-$template = $mustache->loadTemplate('welcomepage');
-echo $template->render(array('firstname' => 'John', 'visitorNumber' => 7));
-?>
